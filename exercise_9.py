830bc00a1812a505848cc9a15cdbb895330f723b
saraducks <saranyadhakshina25@gmail.com>
2016-12-18 23:49:47 -0800

    feat: get input from users

---
+++ exercise_9.py
@@ -0,0 +1,9 @@
+print "What's your name?"
+name = raw_input()                              # asks for user input
+print "your name", name
+print "Do you like python?"
+get_answer = raw_input()
+print "your answer to python question", get_answer
+temp = raw_input("Do you like to play around python? :" )
+print(temp)
+print "Your name is %r , Your answer for python question is %r" % (name, get_answer)
