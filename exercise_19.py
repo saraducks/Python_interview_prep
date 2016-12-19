716ea16357f3bf4ab2cbf6478c8ba31bf0768348
saraducks <saranyadhakshina25@gmail.com>
2016-12-19 13:30:00 -0800

    feat:logical operators

---
+++ exercise_19.py
@@ -0,0 +1,14 @@
+'''
+logical statements, using terminal to execute these lines of code
+'''
+z
+if 3 != 4:                                  # if this condition is true
+    True and not('Test play' == 'test' or 'TEST MATCH' == 'TEST MATCH')   # It will evaluate not and then perform and with True
+
+if 'TEST' == 'test':                       # If they are true, if not execute else part
+    True                                   # execute
+else:
+    True and not(True and False)
+
+if 0 == 0:                                 # if they are true
+    True or not('My Python' != 'MY PYTHON' and 'TEST' == 'test')  # execute
