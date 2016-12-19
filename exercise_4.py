80d79dd6ac9103e250c68f29942c36771e74329e
saraducks <sara@vijays-MacBook-Pro.local>
2016-12-18 22:54:01 -0800

    feat: added playing around python variable
    :
    
    q
    
    wqz1

---
+++ exercise_4.py
@@ -0,0 +1,10 @@
+print "Play around variables and names"
+variable1 = "demo of python"
+variable2 = "This is string"
+concatenate_strings = variable1 + variable2     # this statement will join both the variable1 and varibale2.
+print "concatenation of two strings",concatenate_strings                       #  Here '+' acts as concatenation operator
+number1 = 10
+number2 = 20
+print "Add numbers", number1+number2            #addition of two numbers
+print "You can call any variable using their name: %s" % variable1      # this will print variable1, The %s represents
+                                                                        # string substitution
