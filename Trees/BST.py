64760b2b1a8e0bb2d1a6f8f449f0bc8ddc42f466
saraducks <saranyadhakshina25@gmail.com>
2017-08-14 01:02:09 -0700

    # please include the commit messgae!

--- Trees/BST.py
+++ Trees/BST.py
@@ -1,21 +1,39 @@
 class BST:
-    def __init__(self, data, left_child=None, right_child=None):
+    def __init__(self, data=-1, left_child=None, right_child=None):
         self.node_value = data
         self.left_child = left_child
         self.right_child = right_child
 
     def put(self, input_data):
-        if input_data.node_value < self.node_value:
-            self.left_child = input_data
-        elif input_data.node_value > self.node_value:
-            self.right_child = input_data
+        def put_helper(node, input_data):
+            # check if the left child is None
+            if input_data.node_value < node.node_value:
+                if node.left_child == None:
+                    node.left_child = input_data
+                    print node.left_child.node_value
+                else:
+                    put_helper(node.left_child, input_data)
+            elif input_data.node_value > node.node_value:
+                if node.right_child == None:
+                     node.right_child = input_data
+                     print node.right_child.node_value
+                else:
+                    put_helper(node.right_child, input_data)
+        put_helper(self, input_data)
 
     def print_tree(self):
+        if self.node_value != -1:
             print "root", self.node_value
-            if self.left_child == None:
+
+            if self.left_child != None:
                 print "left", self.left_child.node_value
-            if self.right_child == None:
+                self.left_child.print_tree()
+
+            if self.right_child != None:
                 print "right", self.right_child.node_value
+                self.right_child.print_tree()
+
+
 
 
 B = BST(10)
